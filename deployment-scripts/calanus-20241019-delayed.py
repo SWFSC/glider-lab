812cc4190899a5a990ba54d365c6511cf1fed674
Sam Woodman <sam.woodman@noaa.gov>
2025-03-14 21:12:20 +0000

    cleaning scripts

--- deployment-scripts/calanus-20241019-delayed.py
+++ deployment-scripts/calanus-20241019-delayed.py
@@ -1,12 +1,12 @@
 # Processing script for calanus-20241019
-# This script expected to be run in the glider-utils Instance in GCP
+# This script expects to be run in the glider-utils Instance in GCP
 
 import os
 import logging
 
-import esdglider.pathutils as putils
+# import esdglider.config as config
 import esdglider.gcp as gcp
-import esdglider.config as config
+import esdglider.pathutils as putils
 import esdglider.process as process
 
 
@@ -25,33 +25,29 @@ if __name__ == "__main__":
         level=logging.INFO, 
         datefmt='%Y-%m-%d %H:%M:%S')
 
-
+    # Mount the deployments bucket, and generate paths dictionary
     gcp.gcs_mount_bucket(
         "amlr-gliders-deployments-dev", deployments_path, ro=False)
+    paths = putils.esd_paths(
+        project, deployment, mode, deployments_path, config_path)
     
-    # Create config file - one-time local run by Sam
+    # # Create config file - one-time local run by Sam
     # with open("db/glider-db-prod.txt", "r") as f:
     #     conn_string = f.read()
-
     # config.make_deployment_config(
     #     deployment, project, mode, 
     #     "C:/Users/sam.woodman/Downloads", conn_string)
 
     # Generate timeseries and gridded netCDF files
+    # min_dt determined from examining sci and eng timeseries files
     # 13 Mar 2025: cdom data removed from deployment yaml
     outname_tseng, outname_tssci, outname_1m, outname_5m = process.binary_to_nc(
         deployment, project, mode, deployments_path, config_path, 
         write_timeseries=True, write_gridded=True, 
         min_dt='2024-10-19 17:37:00')
         
-    # Generate profile netCDF files for the DAC
-    # outname_tseng, outname_tssci, outname_1m, outname_5m = process.binary_to_nc(
-    #     deployment, project, mode, deployments_path, config_path, 
-    #     write_timeseries=False, write_gridded=False)
-    
-    # paths = putils.esd_paths(
-    #     project, deployment, mode, deployments_path, config_path)
-    
+    # # Generate profile netCDF files for the DAC
+    # outname_tssci = os.path.join(paths['tsdir'], f"{deployment}-{mode}-sci.nc")
     # process.ngdac_profiles(
     #     outname_tssci, paths['profdir'], paths['deploymentyaml'], 
     #     force=True)
